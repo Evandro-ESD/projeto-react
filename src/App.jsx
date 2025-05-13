19fe4086648f178db2b3f2b8fc6923d1a30eeacd
Evandro <vando86206@gmail.com>
2025-05-13 15:04:12 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>    
+      <header>
+        <h1>Evandro Santos</h1>
+        <p>Aluno CADSPM - I/25 </p>
+      </header>
+      <main>
+        <h2>Sobre min</h2>
+        <p> Lorem ipsum dolor sit amet <strong>consectetur </strong>
+            consectetur adipisicing elit.Molestiae nulla quas amet, 
+            voluptatem facilis temporibus maxime a soluta quidem, esse 
+            eum error consectetur animi neque dolor in magni quae eius?
+        </p>
+        <p> Lorem ipsum dolor sit amet <strong>consectetur </strong>
+            consectetur adipisicing elit.Molestiae nulla quas amet, 
+            voluptatem facilis temporibus maxime a soluta quidem, esse 
+            eum error consectetur animi neque dolor in magni quae eius?
+        </p>
+        <h3>Habilidades Principais</h3>
+         <ul>
+          <li>Montagem e configuração</li>
+          <li>Redes e conectividade</li>
+          <li>Sistemas operacionais</li>
+          <li>Didática técnica para cursos e certificações</li>
+        </ul>
+<br />
+<br />
+<br />
+<br />
+        <div id='simbolos'>
+          <span>&#8943;</span>
+          <span>&#8943;</span>
+          <span>&#8943;</span>
+          <span>&#8943;</span>
+          
+        </div>
+
+      </main>
+      <footer>
+        &copy; 2025 Evandro Santos
+        &there4;	
+      </footer>
+      
+    </>
+  )
+}
+
+export default App
